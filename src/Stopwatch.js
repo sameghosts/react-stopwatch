94c48a88c7fe2b1014f773360337c2baf8f9499c
009kings <009kings@gmail.com>
2021-02-04 11:11:46 -0800

    finished

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -4,24 +4,71 @@ class Stopwatch extends Component {
     constructor() {
         super()
         this.state = {
-            counter: 0
+            counter: null,
+            paused: false,
+            seconds: 0
         }
+    }
+    updateTime = () => {
+        this.setState({
+            seconds: this.state.seconds + 1
+        })
+    }
+
+    resetInterval = () => {
+        clearInterval(this.state.counter)
+        this.setState({
+            counter: null,
+        })
     }
 
     handleStart = e => {
-        setInterval(() => this.setState({
-            counter: this.state.counter + 1
-        }), 1000)
+        this.setState({counter: setInterval(this.updateTime, 1000)})
+    }
+
+    handleReset = e => {
+        this.resetInterval()
+        this.setState({
+            seconds: 0,
+            paused: false
+        })
+    }
+
+    handlePause = e => {
+        // restarting timer
+        if (this.state.paused) {
+            this.handleStart()
+        } else {
+            this.resetInterval()
+        }
+        this.setState({
+            paused: !this.state.paused
+        })
     }
 
     render() {
         return (
             <main className='stopwatch'>
-                <h1>{this.state.counter}</h1>
+                <h1>{this.state.seconds}</h1>
                 <div className='controls'>
-                    <button>Reset</button>
-                    <button onClick={this.handleStart}>Start</button>
-                    <button>Pause</button>
+                    <button 
+                        onClick={this.handleReset}
+                        disabled={this.state.seconds > 0 || this.state.counter ? false : true}
+                    >
+                        Reset
+                    </button>
+                    <button 
+                        onClick={this.handleStart}
+                        disabled={this.state.seconds > 0 || this.state.counter ? true : false}
+                    >
+                        Start
+                    </button>
+                    <button 
+                        onClick={this.handlePause}
+                        disabled={this.state.seconds > 0 || this.state.counter ? false : true}
+                    >
+                        { this.state.paused ? "Restart" : "Pause"}
+                    </button>
                 </div>
             </main>
         )
