ad8e45f7bfc3167471ec6ad2ef0269cb9a976560
sameghosts <seanfeiner.brown@gmail.com>
2021-02-04 14:04:02 -0800

    handle start on click

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react'
-import Stopwatch from './Stopwatch'
-import './App.css';
 
-class App extends Component {
+class Stopwatch extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      counter: 0,
+    }
+    this.handleStart = this.handleStart.bind(this)
+  }
+  handleStart = (e) => {
+    setInterval (function() {
+      let prevCounter = this.state.counter
+      let newCounter = prevCounter + 1
+      return newCounter
+    }, 1000)
+    this.setState({
+      counter: newCounter
+    })
+  }
   render () {
     return (
       <div className="stopwatch">
         <div className="timer-window">
-        <h2>StopWatch Values</h2>
+        <h2>{this.state.counter}</h2>
         </div>
         <div className="button-area">
-          Buttons
+          <button name="reset" onclick={(e) => this.handleStart(e)} value=""> </button>
+          <button name="start" onclick={"handleclick"} value=""> </button>
+          <button name="pause" onclick={"handleclick"} value=""> </button>
         </div>
       </div>
     )
   }
 }
 
-export default App;
+export default Stopwatch;
