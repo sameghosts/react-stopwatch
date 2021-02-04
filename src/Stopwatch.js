4131f2280a69bddcefffd942d9bc23f25a349662
sameghosts <seanfeiner.brown@gmail.com>
2021-02-04 15:59:04 -0800

    start and pause working but insane, refactoring

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react'
 
 class Stopwatch extends Component {
-  constructor(props){
-    super(props)
+  constructor(){
+    super()
     this.state = {
-      counter: 0,
+      timerInit: 0,
+      timerNow: 0,
+      isActive: false,
+      isPaused: false
     }
-    this.handleStart = this.handleStart.bind(this)
+    // this.updateSeconds = this.updateSeconds.bind(this)
   }
-  handleStart = (e) => {
-    setInterval (function() {
-      let prevCounter = this.state.counter
-      let newCounter = prevCounter + 1
-      return newCounter
-    }, 1000)
-    this.setState({
-      counter: newCounter
-    })
+  // updateSeconds = () =>{
+  //   this.setState({
+  //     seconds: +this.state.seconds +1
+  //   })
+  // }
+  handleStart = e => {
+    // this.setState({ isActive: true})
+    // if(this.state.isActive === true){
+      this.setState({
+          isActive: true,
+          timerNow: this.state.timerNow,
+          timerInit: Date.now() - this.state.timerNow
+        })
+      this.timer = setInterval(() => { 
+        this.setState({
+          timerNow: Math.floor((Date.now() - this.state.timerInit)/1000)})
+      }, 1000)
   }
+  resetInterval = () => {
+    clearInterval(this.timer)
+    this.setState((prevState) => ({
+      timerNow: prevState.timerNow
+    }))
+  }
+  handlePause = e => {
+    if (this.state.isPaused){
+      this.handleStart()
+    } else{
+      this.resetInterval()
+    }
+    this.setState((prevState) => ({
+      isPaused: !this.state.isPaused 
+    }))
+  }
+
   render () {
     return (
       <div className="stopwatch">
         <div className="timer-window">
-        <h2>{this.state.counter}</h2>
+        <h2>{this.state.timerNow}</h2>
         </div>
         <div className="button-area">
-          <button name="reset" onclick={(e) => this.handleStart(e)} value=""> </button>
-          <button name="start" onclick={"handleclick"} value=""> </button>
-          <button name="pause" onclick={"handleclick"} value=""> </button>
+          <button name="start" onClick={this.handleStart} value="">Start</button>
+          <button name="pause" onClick={this.handlePause} value=""> pause </button>
+          <button name="pause"  value=""> reset </button>
         </div>
       </div>
     )
